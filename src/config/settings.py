32505ece7de980277e09f7467c0b43d4fe27ceb7
Szymon Płaza <plazik3290@gmail.com>
2022-12-07 16:23:29 +0100

    setup ES and override search, index method

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,6 +2,7 @@ from pydantic import BaseSettings, Field
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from elasticsearch import Elasticsearch
 
 
 class Settings(BaseSettings):
@@ -10,12 +11,16 @@ class Settings(BaseSettings):
     db_name: str = Field(env="DB_NAME")
     db_user: str = Field(env="DB_USER")
     db_password: str = Field(env="DB_PASSWORD")
+    db_host: str = Field(env="DB_HOST")
+    elasticsearch_host: str = Field(env="ELASTICSEARCH_HOST")
 
     @property
     def db_string(self) -> str:
-        return (
-            f"postgresql://{self.db_user}:{self.db_password}@localhost:5432/{self.db_name}"
-        )
+        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:5432/{self.db_name}"
+
+    @property
+    def elasticsearch_string(self) -> str:
+        return f"http://{self.elasticsearch_host}:9200"
 
     class Config:
         env_file = ".env"
@@ -25,6 +30,8 @@ settings = Settings()
 
 
 class DBSession:
+    """Handles base db configuration and session access in repos"""
+
     _session = sessionmaker(bind=create_engine(settings.db_string))
     base = declarative_base()
 
@@ -37,3 +44,16 @@ class DBSession:
             session.rollback()
         finally:
             session.close()
+
+
+class ESClient:
+    """Class for initiating Elasticsearch client"""
+
+    _es = Elasticsearch(settings.elasticsearch_string)
+
+    def search(self, search_data: dict, index: str) -> list:
+        result = self._es.search(index=index, body={"query": {"match": search_data}})
+        return result["hits"]["hits"] # Get nested ES search response
+
+    def index(self, body: dict, index: str):
+        self._es.index(index=index, document=body)
