5470d2801e8b8af136f9eb1863fce3f1d8cd3597
Szymon Płaza <plazik3290@gmail.com>
2022-12-28 16:27:43 +0100

    update account fixture

--- src/fixtures/user_fixture.py
+++ src/fixtures/user_fixture.py
@@ -9,6 +9,7 @@ from config.logger import logger
 session = DBSession().get_session()
 
 user1 = User(
+    id=1,
     name="John",
     surname="Backpack",
     date_of_birth="2000-10-10",
@@ -17,6 +18,7 @@ user1 = User(
     password=Password("zaq1@WSX"),
 )
 user2 = User(
+    id=2,
     name="Daniel",
     surname="Pekov",
     date_of_birth="1990-10-10",
@@ -24,16 +26,38 @@ user2 = User(
     email=Email("test2@bank-app.com"),
     password=Password("ZAQ!2wsx"),
 )
+user3 = User(
+    id=3,
+    name="Central Bank Account",
+    surname="",
+    date_of_birth="1971-12-23",
+    login="bankadmin1",
+    email=Email("bank.admin@bank-app.com"),
+    password=Password("@dm1N098"),
+)
+
 account1 = Account(
     default_currency=Currency("USD"), user_id=1, balance=Decimal("5000.00")
 )
 account2 = Account(
     default_currency=Currency("USD"), user_id=2, balance=Decimal("1000.00")
 )
+account3 = Account(
+    default_currency=Currency("USD"), user_id=3, balance=Decimal("99999999.00")
+)
+account4 = Account(
+    default_currency=Currency("EUR"), user_id=3, balance=Decimal("99999999.00")
+)
+account5 = Account(
+    default_currency=Currency("PLN"), user_id=3, balance=Decimal("99999999.00")
+)
 
 try:
-    session.bulk_save_objects([user1, user2])
-    session.bulk_save_objects([account1, account2])
+    session.query(User).delete()
+    session.query(Account).delete()
+    session.commit()
+    session.bulk_save_objects([user1, user2, user3])
+    session.bulk_save_objects([account1, account2, account3, account4, account5])
     session.commit()
     logger.info("Fixtures uploaded.")
 except IntegrityError:
