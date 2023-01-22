21ade14ed5eca0e73871b34b30e3543c6f73bc8e
Szymon Płaza <plazik3290@gmail.com>
2023-01-22 22:50:25 +0100

    add build to pipeline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='src',
+   version='1.0',
+   description='App build',
+   author='Szymon P.',
+   author_email='szymon.plaza01@gmail.com',
+   packages=['src'],
+)
